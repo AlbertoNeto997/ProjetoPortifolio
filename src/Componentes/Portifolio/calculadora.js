25fcc11001a220fb9c3184b972e0d7599989bdcf
Alberto Nascimento <93000955+albertonsc@users.noreply.github.com>
2023-05-16 10:12:17 -0400

    update calc

--- src/Componentes/Portifolio/calculadora.js
+++ src/Componentes/Portifolio/calculadora.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import Calculadora from '../../assets/Calculadora.png'
-import styled from 'styled-components';
+import React from "react";
+import Calculadora from "../../assets/Calculadora.png";
+import styled from "styled-components";
 
-export default function ImageCalculadora ( ){
+export default function ImageCalculadora() {
+  const ImageCalculadora = styled.nav`
+    transition: 0.5s;
 
-    const ImageCalculadora = styled.nav`
-        transition: 0.5s;
-        
-        :hover{
-            transform: scale(1.1);
-             transition: 0.5s;
-        }
-        img{
-            width: 450px;
-            height:250px;
-            border-radius: 10px;
-        }
-    `;
-    
-    return(
-        <ImageCalculadora>
-            <img  src={Calculadora} alt="logo-dev"/>
-        </ImageCalculadora>
-           
-    )
+    :hover {
+      transform: scale(1.1);
+      transition: 0.5s;
+    }
+    img {
+      width: 450px;
+      height: 250px;
+      border-radius: 10px;
+    }
+  `;
+
+  return (
+    <ImageCalculadora>
+      <a
+        href="https://calculadoraalberto.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={Calculadora} alt="logo-dev" />
+      </a>
+    </ImageCalculadora>
+  );
 }
